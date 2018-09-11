4055e900e9cbc083de1d224dc066b84240c614c2
Daniel Greuter <greuter.daniel@gmail.com>
2018-09-11 21:18:36 +0200

    added test data file and script for raw data import

---
+++ add_raw_data_to_db.sql
@@ -0,0 +1,21 @@
+create schema if not exists import;
+drop table if exists import.master_plan;
+
+create table import.master_plan(
+	start_time_utc text,
+	duration text,
+	date text,
+	team text,
+	spass_type text,
+	target text,
+	request_name text,
+	library_definition text,
+	title text,
+	description text
+);
+
+copy import.master_plan
+-- absolute path neccesary
+from 'XXX/master_plan.csv'
+with delimiter ',' header csv;
+
